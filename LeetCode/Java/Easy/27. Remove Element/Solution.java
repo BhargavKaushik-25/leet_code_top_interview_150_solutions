341dc4dce1a90f829b7700d6d9a3c2ba20e3080f
Bhargav Kaushik <143347645+BhargavKaushik-25@users.noreply.github.com>
2026-09-09 05:10:46 +0530

    Add Easy Java Solution for 27. Remove Element from LeetCode

--- LeetCode/Java/Easy/27. Remove Element/Solution.java
+++ LeetCode/Java/Easy/27. Remove Element/Solution.java
@@ -1,18 +1,14 @@
-class Solution 
-{
-    public int removeElement(int[] nums, int val) 
-    {
+class Solution {
+    //Remove Element solution
+    public int removeElement(int[] nums, int val) {
         int count = 0;
         int k = 0;
-        for(int i = 0;i<nums.length;i++)
-        {
-            if(nums[i] == val)
-            {
+
+        for (int i = 0; i<nums.length; i++){
+            if (nums[i]==val){
                 count++;
-            }
-            else
-            {
-                nums[k] = nums[i];
+            }else{
+                nums[k]=nums[i];
                 k++;
             }
         }
