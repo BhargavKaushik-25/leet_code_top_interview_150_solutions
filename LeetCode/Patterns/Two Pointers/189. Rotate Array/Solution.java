aad618af59cf10a83a4fd1a2e4955a72a65c8ffc
Bhargav Kaushik <143347645+BhargavKaushik-25@users.noreply.github.com>
2026-09-13 05:42:58 +0530

    Add Medium Java Solution for 189. Rotate Array from LeetCode

--- LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.java
+++ LeetCode/Patterns/Two Pointers/189. Rotate Array/Solution.java
@@ -1,11 +1,29 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] n = new int[nums.length];
-        for(int i = 0;i<nums.length;i++){
-            n[(i+k)%nums.length]=nums[i];
-        }
-        for(int i = 0 ;i<nums.length;i++){
-            nums[i]=n[i];
+        k = k % nums.length;
+
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+
+        
+    }
+
+    public static void reverse(int[] nums,int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
+
+    // int[] n = new int[nums.length];
+        // for(int i = 0;i<nums.length;i++){
+        //     n[(i+k)%nums.length]=nums[i];
+        // }
+        // for(int i = 0 ;i<nums.length;i++){
+        //     nums[i]=n[i];
+        // }
 }
